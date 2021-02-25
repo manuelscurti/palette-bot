8482762f4b9aba324dee48d31bd5cfb134b299fb
mscurti <manuel.scurti.dev@gmail.com>
2021-02-25 00:18:27 +0000

    added basic error handler and action decorator

--- bot.py
+++ bot.py
@@ -8,9 +8,14 @@ from telegram.ext import (
     MessageHandler,
     Filters,
     ConversationHandler,
-    CallbackContext,
+    CallbackContext
 )
+from telegram import ParseMode, ChatAction
+from telegram.utils.helpers import mention_html
+import sys
+import traceback
 import paletter
+from functools import wraps
 
 # Enable logging
 logging.basicConfig(
@@ -19,6 +24,54 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+def send_action(action):
+    """Sends `action` while processing func command."""
+
+    def decorator(func):
+        @wraps(func)
+        def command_func(update, context, *args, **kwargs):
+            context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=action)
+            return func(update, context,  *args, **kwargs)
+        return command_func
+    
+    return decorator
+
+def error(update, context):
+    # add all the dev user_ids in this list. You can also add ids of channels or groups.
+    devs = [264217449]
+    # we want to notify the user of this problem. This will always work, but not notify users if the update is an 
+    # callback or inline query, or a poll update. In case you want this, keep in mind that sending the message 
+    # could fail
+    if update.effective_message:
+        text = "Hey. I'm sorry to inform you that an error happened while I tried to handle your update. " \
+               "My developer(s) will be notified."
+        update.effective_message.reply_text(text)
+    # This traceback is created with accessing the traceback object from the sys.exc_info, which is returned as the
+    # third value of the returned tuple. Then we use the traceback.format_tb to get the traceback as a string, which
+    # for a weird reason separates the line breaks in a list, but keeps the linebreaks itself. So just joining an
+    # empty string works fine.
+    trace = "".join(traceback.format_tb(sys.exc_info()[2]))
+    # lets try to get as much information from the telegram update as possible
+    payload = ""
+    # normally, we always have an user. If not, its either a channel or a poll update.
+    if update.effective_user:
+        payload += f' with the user {mention_html(update.effective_user.id, update.effective_user.first_name)}'
+    # there are more situations when you don't get a chat
+    if update.effective_chat:
+        payload += f' within the chat <i>{update.effective_chat.title}</i>'
+        if update.effective_chat.username:
+            payload += f' (@{update.effective_chat.username})'
+    # but only one where you have an empty payload by now: A poll (buuuh)
+    if update.poll:
+        payload += f' with the poll id {update.poll.id}.'
+    # lets put this in a "well" formatted text
+    text = f"Hey.\n The error <code>{context.error}</code> happened{payload}. The full traceback:\n\n<code>{trace}" \
+           f"</code>"
+    # and send it to the dev(s)
+    for dev_id in devs:
+        context.bot.send_message(dev_id, text, parse_mode=ParseMode.HTML)
+    # we raise the error again, so the logger module catches it. If you don't use the logger module, use it.
+    raise
 
 def start(update: Update, context: CallbackContext):
     update.message.reply_text(
@@ -27,6 +80,7 @@ def start(update: Update, context: CallbackContext):
     )
 
 
+@send_action(ChatAction.TYPING)
 def wait_picture(update: Update, context: CallbackContext) -> int:
      user = update.message.from_user
      photo_file = update.message.photo[-1].get_file()
@@ -77,6 +131,8 @@ def cancel(update: Update, context: CallbackContext) -> int:
 
 def main() -> None:
     # Create the Updater and pass it your bot's token.
+
+
     updater = Updater("1643470843:AAHto026moyTLXUyXEd0nFcv4-HF_paS-Ic")
 
     # Get the dispatcher to register handlers

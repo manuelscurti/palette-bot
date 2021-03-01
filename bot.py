b3eba13e7bcb2f59cc283f646550b2b2bd8bcacf
manuelscurti <manuel9scurti@gmail.com>
2021-03-01 00:00:36 +0000

    refactoring into classes

--- bot.py
+++ bot.py
@@ -15,13 +15,11 @@ from telegram.utils.helpers import mention_html
 import sys
 import traceback
 import paletter
+from paletter import PaletteService, ScikitClusteringService, MatplotlibImageService
 from functools import wraps
 
 # Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
-
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def send_action(action):
@@ -36,121 +34,61 @@ def send_action(action):
     
     return decorator
 
-def error(update, context):
-    # add all the dev user_ids in this list. You can also add ids of channels or groups.
-    devs = [264217449]
-    # we want to notify the user of this problem. This will always work, but not notify users if the update is an 
-    # callback or inline query, or a poll update. In case you want this, keep in mind that sending the message 
-    # could fail
-    if update.effective_message:
-        text = "Hey. I'm sorry to inform you that an error happened while I tried to handle your update. " \
-               "My developer(s) will be notified."
-        update.effective_message.reply_text(text)
-    # This traceback is created with accessing the traceback object from the sys.exc_info, which is returned as the
-    # third value of the returned tuple. Then we use the traceback.format_tb to get the traceback as a string, which
-    # for a weird reason separates the line breaks in a list, but keeps the linebreaks itself. So just joining an
-    # empty string works fine.
-    trace = "".join(traceback.format_tb(sys.exc_info()[2]))
-    # lets try to get as much information from the telegram update as possible
-    payload = ""
-    # normally, we always have an user. If not, its either a channel or a poll update.
-    if update.effective_user:
-        payload += f' with the user {mention_html(update.effective_user.id, update.effective_user.first_name)}'
-    # there are more situations when you don't get a chat
-    if update.effective_chat:
-        payload += f' within the chat <i>{update.effective_chat.title}</i>'
-        if update.effective_chat.username:
-            payload += f' (@{update.effective_chat.username})'
-    # but only one where you have an empty payload by now: A poll (buuuh)
-    if update.poll:
-        payload += f' with the poll id {update.poll.id}.'
-    # lets put this in a "well" formatted text
-    text = f"Hey.\n The error <code>{context.error}</code> happened{payload}. The full traceback:\n\n<code>{trace}" \
-           f"</code>"
-    # and send it to the dev(s)
-    for dev_id in devs:
-        context.bot.send_message(dev_id, text, parse_mode=ParseMode.HTML)
-    # we raise the error again, so the logger module catches it. If you don't use the logger module, use it.
-    raise
+class BotService:
+    def __init__(self, token: str, palette_service: PaletteService):
+        self.token=token
+        self.palette_service=palette_service
 
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        'Hey! I\'m a super smart palette generator. Try me 👀\n'
-        'Send a picture to visualize the most dominant colors. or send /help if you need it\n'
-    )
+    def photo_handler(self, update: Update, context: CallbackContext):
+        context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=ChatAction.TYPING)
 
+        user = update.message.from_user
+        photo_file = update.message.photo[-1].get_file()
 
-@send_action(ChatAction.TYPING)
-def wait_picture(update: Update, context: CallbackContext) -> int:
-     user = update.message.from_user
-     photo_file = update.message.photo[-1].get_file()
+        input_path = "pics/"+str(user.username) + "_" + time.strftime("%Y%m%d%H%M%S")
+        output_path = input_path + "_processed.png"
 
-     timestr = time.strftime("%Y%m%d-%H%M%S")
-     input_filename = str(user.username)+"_"+timestr
-     photo_file.download(input_filename)
-     logger.info("Photo of %s: %s", user.first_name, input_filename)
+        photo_file.download(input_path)
+        logger.info("Photo of %s: %s", user.first_name, input_path)
 
-     #process photo
-     output_filename = paletter.generate_palette_from_image(input_filename, expected_colors=5, width=300, height=300, dpi=1200)
+        palette_service.generate(input_path, output_path)
 
-     # send result back
-     send_palette(update, context, output_filename) 
+        file_handler = open(output_path, 'rb')
 
-     clear_files(input_filename, output_filename)
+        context.bot.send_photo(chat_id=update.message.chat_id, photo=file_handler)
 
-def clear_files(input_filename: str, output_filename: str):
-     if os.path.exists(input_filename):
-          os.remove(input_filename)
+        update.message.reply_text('Here you go! 👨🏽‍🎨')
 
-     if os.path.exists(output_filename):
-          os.remove(output_filename)
+        if os.path.exists(input_path):
+            os.remove(input_path)
 
-def get_help(update: Update, context: CallbackContext) -> int:
-     
-     update.message.reply_text(
-        'TODO: write help here'
-     )
+        if os.path.exists(output_path):
+            os.remove(output_path)
 
-def send_palette(update: Update, context: CallbackContext, output_filename: str):
-     file_handler = open(output_filename, 'rb')
+    def start_handler(self, update: Update, context: CallbackContext):
+        update.message.reply_text(
+            'Hey! I\'m a super smart palette generator. Try me 👀\n'
+            'Send a picture to visualize the most dominant colors. or send /help if you need it\n'
+        )
 
-     #context.bot.send_document(chat_id=update.message.chat_id, thumb=file_handler, document=file_handler)
-     context.bot.send_photo(chat_id=update.message.chat_id, photo=file_handler)
+    def help_handler(self, update: Update, context: CallbackContext):
+        update.message.reply_text('TODO: write help here')
 
-     update.message.reply_text(
-        'Here you go! 👨🏽‍🎨'
-     )
+    def start(self):
+        updater = Updater(self.token)
 
-def cancel(update: Update, context: CallbackContext) -> int:
-    user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
-    update.message.reply_text(
-        'Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove()
-    )
+        dispatcher = updater.dispatcher
 
+        dispatcher.add_handler(MessageHandler(Filters.photo, self.photo_handler))
+        dispatcher.add_handler(CommandHandler('start', self.start_handler))
+        dispatcher.add_handler(CommandHandler('help', self.help_handler))
 
-def main() -> None:
-    # Create the Updater and pass it your bot's token.
+        updater.start_polling()
 
-
-    updater = Updater(os.environ['BOT_TOKEN'])
-
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
-
-    dispatcher.add_handler(MessageHandler(Filters.photo, wait_picture))
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(CommandHandler('help', get_help))
-    dispatcher.add_handler(CommandHandler('cancel', cancel))
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
-
+        updater.idle()
 
 if __name__ == '__main__':
-    main()
+    palette_service = PaletteService(ScikitClusteringService(), MatplotlibImageService())
+
+    bot = BotService(os.environ["BOT_TOKEN"], palette_service)
+    bot.start()
--- paletter.py
+++ paletter.py
@@ -2,7 +2,88 @@ from sklearn.cluster import MiniBatchKMeans
 import matplotlib.pyplot as plt
 import argparse
 import utils
+import numpy as np
 import cv2
+
+class IClusteringService:
+	def fit(self, data, n_clusters): pass
+	def get_histogram(self, clusters, weighted: bool, height: int, width: int): pass
+
+class IImageService:
+	def get_pixels_list(self, image_path: str): pass
+	def save(self, image, dpi: int, path: str): pass
+
+class MatplotlibImageService(IImageService):
+	def get_pixels_list(self, image_path: str):
+		image = cv2.imread(image_path)
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+		image = image.reshape((image.shape[0] * image.shape[1], 3))
+		return image
+
+	def save(self, image_array, dpi: int, path: str):
+		plt.figure()
+		plt.axis("off")
+		plt.imshow(image_array)
+		plt.savefig(path, dpi=dpi, bbox_inches='tight', pad_inches = 0)
+
+
+class ScikitClusteringService(IClusteringService):
+	def fit(self, data, n_clusters): 
+		clusters = MiniBatchKMeans(n_clusters = n_clusters)
+		clusters.fit(data)
+		return clusters 
+
+	def get_histogram(self, clusters, weighted: bool, height: int, width: int):
+		num_labels = np.arange(0, len(np.unique(clusters.labels_)) + 1)
+		hist, _ = np.histogram(clusters.labels_, bins = num_labels)
+		hist = hist.astype("float")
+		hist /= hist.sum()
+
+		if not weighted:
+			return self.__plot_uniform_bars(hist, clusters.cluster_centers_, width, height)
+
+		return self.__plot_weighted_bars(hist, clusters.cluster_centers_, width, height)
+
+	def __plot_uniform_bars(self, hist, centroids, width, height):
+		bar = np.zeros((height, width, 3), dtype = "uint8")
+		start_x = 0
+		num_labels = len(centroids) 
+		bar_width = width / num_labels
+
+		for (percent, color) in zip(hist, centroids):
+			end_x = start_x + bar_width
+			cv2.rectangle(bar, (int(start_x), 0), (int(end_x), height), color.astype("uint8").tolist(), -1)
+			start_x = end_x
+
+		return bar
+
+	def __plot_weighted_bars(self, hist, centroids, width, height):
+		bar = np.zeros((height, width, 3), dtype = "uint8")
+		start_x = 0
+
+		for (percent, color) in zip(hist, centroids):
+			end_x = start_x + (percent * width)
+			cv2.rectangle(bar, (int(start_x), 0), (int(end_x), height), color.astype("uint8").tolist(), -1)
+			start_x = end_x
+
+		return bar
+
+class PaletteService:
+	def __init__(self, clustering_service: IClusteringService, image_service: IImageService):
+		self.clustering_service = clustering_service
+		self.image_service = image_service
+		# telegram APIs allow for a limited combination of h x w x dpi as a final output
+	
+	def generate(self, input_path: str, output_path: str, width=300, height=300, dpi=1200, colors=5, weighted_palette=False):
+		pixels = self.image_service.get_pixels_list(input_path)
+
+		clusters = self.clustering_service.fit(pixels, colors)
+		histogram = self.clustering_service.get_histogram(clusters, weighted_palette, height, width)
+
+		self.image_service.save(histogram, dpi, output_path)
+		
+	
+
 
 def extract_palette(image_path: str, n_clusters: int):
 	image = cv2.imread(image_path)
